7d27da639638bcfc98882c42bcb919737edb76ba
Matt Dudzik <mdudzik92@gmail.com>
2021-07-23 22:08:02 -0500

    Set up GET, POST, and DELETE routes

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
 	res.send("GET transactions");
 };
+
+// Add transaction
+// POST Route: /api/v1/transactions
+// Public
+exports.addTransaction = (req, res, next) => {
+	res.send("POST transaction");
+};
+
+// Delete transactions
+// DELETE Route: /api/v1/transactions/:id
+// Public
+exports.deleteTransaction = (req, res, next) => {
+	res.send("DELETE transaction");
+};
