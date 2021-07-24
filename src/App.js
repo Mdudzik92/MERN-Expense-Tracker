e87f19f6c06a81418af82d358caeb0ed10e3477e
Matt Dudzik <mdudzik92@gmail.com>
2021-07-23 20:35:15 -0500

    Added action in global state to add a transaction by returning transactions that are already there in addition to the ones in the payload by using spread operator in a new array, updated README

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import React from "react";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
@@ -6,6 +6,8 @@ import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
 
 import { GlobalProvider } from "./context/GlobalState";
+
+import "./App.css";
 
 function App() {
 	return (
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,14 +1,11 @@
+// Global state gets passed down to any components that need it
+
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 // Initial state
 const initialState = {
-	transactions: [
-		{ id: 1, text: "Flower", amount: -20 },
-		{ id: 2, text: "Salary", amount: 300 },
-		{ id: 3, text: "Book", amount: -10 },
-		{ id: 4, text: "Camera", amount: 150 },
-	],
+	transactions: [],
 };
 
 // Create context
@@ -18,11 +15,18 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(AppReducer, initialState);
 
-	// Actions making calls to the reducer
+	// Actions
 	function deleteTransaction(id) {
 		dispatch({
 			type: "DELETE_TRANSACTION",
 			payload: id,
+		});
+	}
+
+	function addTransaction(transaction) {
+		dispatch({
+			type: "ADD_TRANSACTION",
+			payload: transaction,
 		});
 	}
 
@@ -31,6 +35,7 @@ export const GlobalProvider = ({ children }) => {
 			value={{
 				transactions: state.transactions,
 				deleteTransaction,
+				addTransaction,
 			}}
 		>
 			{children}
