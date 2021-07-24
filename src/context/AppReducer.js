07f564960a3f2664a610004626cf7c307caceada
Matt Dudzik <mdudzik92@gmail.com>
2021-07-23 19:18:11 -0500

    Accesing each transaction value through the transaction component, specifying state changes with AppReducer

---
+++ src/context/AppReducer.js
@@ -0,0 +1,8 @@
+// Specifying the application state changes in response to the store, to the context
+
+export default (state, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
