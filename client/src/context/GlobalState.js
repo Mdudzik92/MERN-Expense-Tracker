b38463dc6ffa356c4ad0ec886091ba19ea877924
Matt Dudzik <mdudzik92@gmail.com>
2021-07-25 19:55:51 -0500

    Utilizing actions from TransactionList.js to fetch transactions from back end, using morgan to log requests

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -2,10 +2,13 @@
 
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // Initial state
 const initialState = {
 	transactions: [],
+	error: null,
+	loading: true,
 };
 
 // Create context
@@ -16,6 +19,22 @@ export const GlobalProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(AppReducer, initialState);
 
 	// Actions
+	async function getTransactions() {
+		try {
+			const res = await axios.get("/api/v1/transactions");
+
+			dispatch({
+				type: "GET_TRANSACTIONS",
+				payload: res.data.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: "TRANSACTION_ERROR",
+				payload: err.response.data.error,
+			});
+		}
+	}
+
 	function deleteTransaction(id) {
 		dispatch({
 			type: "DELETE_TRANSACTION",
@@ -34,6 +53,9 @@ export const GlobalProvider = ({ children }) => {
 		<GlobalContext.Provider
 			value={{
 				transactions: state.transactions,
+				error: state.error,
+				loading: state.loading,
+				getTransactions,
 				deleteTransaction,
 				addTransaction,
 			}}
